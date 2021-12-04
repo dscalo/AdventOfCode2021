01b8d3f734d77605fca535edb9e2c3e6fe9800e2
dscalo <dansbrood@live.com>
2021-12-03 21:31:28 -0500

    refactoring

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -1,23 +1,19 @@
 extern crate file_reader;
 use file_reader::read_file;
 
-type Report = Vec<Vec<u32>>;
+type Report = Vec<Vec<char>>;
 
 #[derive(Debug)]
-struct PosCt(u32, u32);
+struct BitsCount(u32, u32);
 
-fn parse_bits(s: &str, report: &mut Report) {
-    let row = s
-        .chars()
-        .map(|c| c.to_digit(10).unwrap())
-        .collect::<Vec<u32>>();
-    report.push(row);
+fn parse_bits(s: &str, report: &mut Report) {  
+    report.push( s.chars().collect::<Vec<char>>());
 }
 
 fn part_1(report: &Report) -> u32 {
-    let mut pos: Vec<PosCt> = Vec::new();
+    let mut pos: Vec<BitsCount> = Vec::new();
     for _ in 0..report[0].len() {
-        pos.push(PosCt(0, 0));
+        pos.push(BitsCount(0, 0));
     }
 
     let mut gamma = "".to_string();
@@ -26,9 +22,9 @@ fn part_1(report: &Report) -> u32 {
     for row in report {
         for b in 0..row.len() {
             match row[b] {
-                0 => pos[b].0 += 1,
-                1 => pos[b].1 += 1,
-                _ => panic!("{}", "invalid number"),
+                '0' => pos[b].0 += 1,
+                '1' => pos[b].1 += 1,
+                _ => panic!("{}", "invalid char"),
             };
         }
     }
@@ -51,19 +47,19 @@ fn part_1(report: &Report) -> u32 {
     g * e
 }
 
-fn bit_freq(p: usize, report: &Report) -> PosCt {
-    let mut pos = PosCt(0, 0);
+fn bit_freq(p: usize, report: &Report) -> BitsCount {
+    let mut pos = BitsCount(0, 0);
     for row in report {
         match row[p] {
-            0 => pos.0 += 1,
-            1 => pos.1 += 1,
+            '0' => pos.0 += 1,
+            '1' => pos.1 += 1,
             _ => panic!("{}", "invalid number"),
         }
     }
     pos
 }
 
-fn filter_report(p: usize, keep: u32, report: &Report) -> Report {
+fn filter_report(p: usize, keep: char, report: &Report) -> Report {
     let mut filtered = Report::new();
 
     for row in report {
@@ -82,9 +78,9 @@ fn part_2(report: &Report) -> u32 {
     while oxygen.len() > 1 {
         for p in 0..row_size {
             let ct = bit_freq(p, &oxygen);
-            let mut keep = 1;
+            let mut keep = '1';
             if ct.0 > ct.1 {
-                keep = 0;
+                keep = '0';
             }
             oxygen = filter_report(p, keep, &oxygen);
             if oxygen.len() == 1 {
@@ -96,9 +92,9 @@ fn part_2(report: &Report) -> u32 {
     while c02.len() > 1 {
         for p in 0..row_size {
             let ct = bit_freq(p, &c02);
-            let mut keep = 0;
+            let mut keep = '0';
             if ct.1 < ct.0 {
-                keep = 1;
+                keep = '1';
             }
             c02 = filter_report(p, keep, &c02);
             if c02.len() == 1 {
@@ -106,16 +102,8 @@ fn part_2(report: &Report) -> u32 {
             }
         }
     }
-    let o_str = oxygen[0]
-        .clone()
-        .into_iter()
-        .map(|i| i.to_string())
-        .collect::<String>();
-    let c_str = c02[0]
-        .clone()
-        .into_iter()
-        .map(|i| i.to_string())
-        .collect::<String>();
+    let o_str = oxygen[0].iter().collect::<String>();
+    let c_str = c02[0].iter().collect::<String>();
 
     println!("o: {}, c: {}", o_str, c_str);
     let o = u32::from_str_radix(&o_str, 2).unwrap();
@@ -131,5 +119,5 @@ fn main() {
     let ans_1 = part_1(&report);
     let ans_2 = part_2(&report);
     println!("Part 1: {}", ans_1);
-    println!("Part 2: {}", ans_2);
+    println!("Part 2: {}", ans_2);    
 }
---
+++ day-4/src/main.rs
@@ -0,0 +1,6 @@
+extern crate file_reader;
+use file_reader::read_file;
+
+fn main() {
+    println!("Hello, world!");
+}
