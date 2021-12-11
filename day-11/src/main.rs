d3662041457b16be22126cbdc485a0eb4a342410
dscalo <dansbrood@live.com>
2021-12-11 01:09:57 -0500

    code cleanup

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -12,91 +12,91 @@ fn print_grid(grid: &Grid) {
 
 fn parse_grid(s: &str, grid: &mut Grid) {
     let row = s
-    .chars()
-    .map(|c| c.to_string().parse::<i32>().unwrap())
-    .collect::<Vec<i32>>();
-   grid.push(row)
+        .chars()
+        .map(|c| c.to_string().parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+    grid.push(row)
 }
 
 fn increment_adjacent(grid: &mut Grid, y: usize, x: usize) -> u32 {
     let mut flashes = 0;
     // top left y-1, x-1
-    if y as i32 - 1 >= 0 && x as i32 - 1 >= 0 && grid[y-1][x-1] > 0 {
-        grid[y-1][x-1] += 1;
-        if grid[y-1][x-1] > 9 {
-            grid[y-1][x-1] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y-1, x-1)
+    if y as i32 - 1 >= 0 && x as i32 - 1 >= 0 && grid[y - 1][x - 1] > 0 {
+        grid[y - 1][x - 1] += 1;
+        if grid[y - 1][x - 1] > 9 {
+            grid[y - 1][x - 1] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y - 1, x - 1)
         }
     }
 
-    // top y-1 
-    if y as i32 - 1 >= 0 && grid[y-1][x] > 0 {
-        grid[y-1][x] += 1;
-        if grid[y-1][x] > 9 {
-            grid[y-1][x] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y-1, x)
+    // top y-1
+    if y as i32 - 1 >= 0 && grid[y - 1][x] > 0 {
+        grid[y - 1][x] += 1;
+        if grid[y - 1][x] > 9 {
+            grid[y - 1][x] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y - 1, x)
         }
     }
 
     // top right y-1 x+1
-    if y as i32 - 1 >= 0 && x + 1 < grid[y].len() && grid[y-1][x+1] > 0 {
-        grid[y-1][x+1] += 1;
-        if grid[y-1][x+1] > 9 {
-            grid[y-1][x+1] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y-1, x+1)
+    if y as i32 - 1 >= 0 && x + 1 < grid[y].len() && grid[y - 1][x + 1] > 0 {
+        grid[y - 1][x + 1] += 1;
+        if grid[y - 1][x + 1] > 9 {
+            grid[y - 1][x + 1] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y - 1, x + 1)
         }
     }
 
     // right x+1
-    if x + 1 < grid[y].len() && grid[y][x+1] > 0 {
-        grid[y][x+1] += 1;
-        if grid[y][x+1] > 9 {
-            grid[y][x+1] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y, x+1)
+    if x + 1 < grid[y].len() && grid[y][x + 1] > 0 {
+        grid[y][x + 1] += 1;
+        if grid[y][x + 1] > 9 {
+            grid[y][x + 1] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y, x + 1)
         }
     }
 
     // bottom right y+1 x+1
-    if y + 1 < grid.len() && x + 1 < grid[y].len() && grid[y+1][x+1] > 0 {
-        grid[y+1][x+1] += 1;
-        if grid[y+1][x+1] > 9 {
-            grid[y+1][x+1] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y+1, x+1)
+    if y + 1 < grid.len() && x + 1 < grid[y].len() && grid[y + 1][x + 1] > 0 {
+        grid[y + 1][x + 1] += 1;
+        if grid[y + 1][x + 1] > 9 {
+            grid[y + 1][x + 1] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y + 1, x + 1)
         }
     }
 
-     // bottom y+1 
-     if y + 1 < grid.len() && grid[y+1][x] > 0 {
-        grid[y+1][x] += 1;
-        if grid[y+1][x] > 9 {
-            grid[y+1][x] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y+1, x)
+    // bottom y+1
+    if y + 1 < grid.len() && grid[y + 1][x] > 0 {
+        grid[y + 1][x] += 1;
+        if grid[y + 1][x] > 9 {
+            grid[y + 1][x] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y + 1, x)
         }
     }
 
-     // bottom left y+1, x-1
-     if  y + 1 < grid.len() && x as i32 - 1 >= 0 && grid[y+1][x-1] > 0 {
-        grid[y+1][x-1] += 1;
-        if grid[y+1][x-1] > 9 {
-            grid[y+1][x-1] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y+1, x-1)
+    // bottom left y+1, x-1
+    if y + 1 < grid.len() && x as i32 - 1 >= 0 && grid[y + 1][x - 1] > 0 {
+        grid[y + 1][x - 1] += 1;
+        if grid[y + 1][x - 1] > 9 {
+            grid[y + 1][x - 1] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y + 1, x - 1)
         }
     }
 
-     // left x-1
-     if x as i32 - 1 >= 0 && grid[y][x-1] > 0 {
-        grid[y][x-1] += 1;
-        if grid[y][x-1] > 9 {
-            grid[y][x-1] = 0;
-            flashes +=1;
-            flashes += increment_adjacent(grid, y, x-1)
+    // left x-1
+    if x as i32 - 1 >= 0 && grid[y][x - 1] > 0 {
+        grid[y][x - 1] += 1;
+        if grid[y][x - 1] > 9 {
+            grid[y][x - 1] = 0;
+            flashes += 1;
+            flashes += increment_adjacent(grid, y, x - 1)
         }
     }
 
@@ -116,37 +116,32 @@ fn step(grid: &mut Grid) -> u32 {
     increase_energy_levels(grid);
 
     for y in 0..grid.len() {
-        for x in 0..grid[y].len() {          
+        for x in 0..grid[y].len() {
             if grid[y][x] > 9 {
                 grid[y][x] = 0;
                 flashes += 1;
-               flashes += increment_adjacent(grid, y, x)
+                flashes += increment_adjacent(grid, y, x)
             }
-
         }
     }
     flashes
-
 }
 
-fn part_1(grid: &mut Grid, steps: u32) -> u32 {   
+fn part_1(grid: &mut Grid, steps: u32) -> u32 {
     let mut flashes = 0;
-   
     for _ in 0..steps {
-        flashes += step(grid);       
+        flashes += step(grid);
     }
 
     flashes
 }
 
-fn part_2(grid: &mut Grid) -> u32 {    
+fn part_2(grid: &mut Grid) -> u32 {
     let mut counter = 0;
 
     loop {
-       
         let flashes = step(grid);
         counter += 1;
-       
         if flashes == 100 {
             break;
         }
@@ -156,13 +151,12 @@ fn part_2(grid: &mut Grid) -> u32 {
 }
 
 fn main() {
-   let mut grid: Grid = Grid::new();
-  
+    let mut grid: Grid = Grid::new();
 
-   read_file("puzzle.txt", parse_grid, &mut grid);
+    read_file("puzzle.txt", parse_grid, &mut grid);
 
-   let mut p2_grid = grid.clone();
-   //print_grid(&grid);
+    let mut p2_grid = grid.clone();
+    //print_grid(&grid);
 
     let ans_1 = part_1(&mut grid, 100);
     println!("Part 1: {}", ans_1);
